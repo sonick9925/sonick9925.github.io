8833d05bed886cf6ac7806d95ea4120f3cf0c3cc
sonick9925 <149050546+sonick9925@users.noreply.github.com>
2025-01-25 17:46:18 +0200

    Add files via upload

---
+++ 52/18.js
@@ -0,0 +1,36 @@
+let divFon = document.querySelector('.oclock')
+let pHour = document.querySelector('.p-hour')
+let pDay = document.querySelector('.p-day')
+function Time() {
+    let main = new Date();
+    console.log(main)
+    let hour = main.getHours()
+    let hv = main.getMinutes()
+    let sknd = main.getSeconds()
+    sknd = sknd < 10 ? '0' + sknd : sknd
+    hv = hv < 10 ? '0' + hv : hv
+    pHour.innerHTML = hour + ':' + hv + ':' + sknd
+    let day = main.getDay()
+    let nameDay = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', "П'ятниця", 'Субота']
+    let num = main.getDate()
+    num = num < 10 ? '0' + num : num
+    let mnth = main.getMonth() + 1
+    mnth = mnth < 10 ? '0' + mnth : mnth
+    pDay.innerHTML = nameDay[day] + ' , ' + num + '.' +  mnth +'.' + main.getFullYear()
+    if (hour >= 6 && hour <= 11) {
+        divFon.style.backgroundImage = 'url(img/morning.jpg)';
+    }
+    else if (hour > 11 && hour <= 18) {
+        divFon.style.backgroundImage = 'url(img/day2.jpg)'
+    }
+    else if (hour > 18 && hour <= 20) {
+        divFon.style.backgroundImage = 'url(img/evening.jpg)'
+    }
+    else if (hour > 20 || hour < 6) {
+        divFon.style.backgroundImage = 'url(img/night2.jpg)'
+    }
+}
+window.addEventListener('load', function () { Time(); })
+setInterval(() => {
+    Time()
+}, 1000)
